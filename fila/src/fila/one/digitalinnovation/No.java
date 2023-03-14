d7196e3f3bb211444baa97bf17fa6ebc8a126da4
DESKTOP-4REKP5G\itson <itsonsantos@gmail.com>
2023-03-13 21:43:14 -0300

    implementação dos métodos das estruturas de dados stack, queue e list.

---
+++ fila/src/fila/one/digitalinnovation/No.java
@@ -0,0 +1,35 @@
+package fila.one.digitalinnovation;
+
+public class No<T> {
+
+    private T object;
+    private No<T> refNo;
+
+    public No(T object) {
+        this.refNo = null;
+        this.object = object;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public void setObject(T object) {
+        this.object = object;
+    }
+
+    public No<T> getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dado=" + object +
+                '}';
+    }
+}
